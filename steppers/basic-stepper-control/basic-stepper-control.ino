1aa88412e3443124f8dbb8997590cf70e57eed10
Alexander Twilla <alexandertwilla@gmail.com>
2019-03-11 17:58:47 -0400

    Add serial control functionality.

--- steppers/basic-stepper-control/basic-stepper-control.ino
+++ steppers/basic-stepper-control/basic-stepper-control.ino
@@ -1,63 +1,4 @@
-#include "StepperMotor.h"
-
-/*class Stepper {
-  public:
-    Stepper(int enablePin, int pulsePin, int dirPin);
-    void stepCW();
-    void stepCCW();
-    void disable();
-    void enable();
-
-  private:
-    int enablePin;
-    int pulsePin;
-    int dirPin;
-};
-
-Stepper::Stepper(int enablePin, int pulsePin, int dirPin) {
-    // Constructor for Stepper class. Given pins will be enabled 
-    // as output pins.
-   
-    this->enablePin = enablePin;
-    this->pulsePin = pulsePin;
-    this->dirPin = dirPin;
-    
-    // Reduce code required to use stepper.
-    pinMode(enablePin, OUTPUT);
-    pinMode(pulsePin, OUTPUT);
-    pinMode(dirPin, OUTPUT);
-
-    // Enable pin disables driver on HIGH.
-    digitalWrite(enablePin, LOW);
-    digitalWrite(pulsePin, LOW);
-}
-
-void Stepper::stepCW() {
-
-    int pulseDelay = 100; // This equals 0.5 * Period.
-    digitalWrite(dirPin, HIGH); //Spins CW w/ respect to back by default.
-
-    // Driver suggests delay between setting direction and driving.
-    delayMicroseconds(10);
-
-    digitalWrite(pulsePin, HIGH);
-    delayMicroseconds(pulseDelay);
-    digitalWrite(pulsePin, LOW);
-    delayMicroseconds(pulseDelay);
-}
-
-void Stepper::stepCCW() {
-
-    int pulseDelay = 100;
-    digitalWrite(dirPin, LOW);
-
-    delayMicroseconds(10);
-
-    digitalWrite(pulsePin, HIGH);
-    delayMicroseconds(pulseDelay);
-    digitalWrite(pulsePin, LOW);
-    delayMicroseconds(pulseDelay);
-}*/
+#include "Stepper.h"
 
 // define pins numbers
 const int rightPin = 12;
@@ -66,16 +7,36 @@ const int enablePin = 8;
 const int pulsePin = 2;
 const int dirPin = 5;
 
+int serialCtrl = 0;
+
 Stepper stepper1(enablePin, pulsePin, dirPin);
 
 void setup() {
-
+  Serial.begin(2000000);
   // Enable control pins.
   pinMode(rightPin, INPUT);
   pinMode(leftPin, INPUT);
 }
 
 void loop() {
+
+  if (Serial.available() > 0) {
+    serialCtrl = Serial.parseInt();
+  }
+
+  int startTime = millis();
+  
+  if (serialCtrl == 1) {
+    while (millis() - startTime < 1000) {
+      stepper1.stepCW();
+    }
+  }
+  
+  if (serialCtrl == 2) {
+    while (millis() - startTime < 1000) {
+      stepper1.stepCCW();
+    }
+  }
   
   while (digitalRead(rightPin) == HIGH) {
     //Rotate motor clockwise while rightPin pressed.
