de1ba1981b044c7aee008dd87599ca1260a10798
Alexander Twilla <alexandertwilla@gmail.com>
2019-03-29 08:06:51 -0400

    Replace kivy code with tkinter.

--- GUI/main.py
+++ GUI/main.py
@@ -1,36 +1,19 @@
 import tkinter
 import serial
+from tkinter.ttk import Frame, Button, Style
 
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+class ControlApp(Frame):
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.grid()
+		self.addWidgets()
 
-class CurrentSensorReadings(GridLayout):
-	#super(CurrentSensorReadings, self).__init__(**kwargs)
+	def addWidgets(self):
+		self.exitButton = Button(self, text="Exit Program", 
+			command=self.quit)
+		self.exitButton.grid()
 
-	def __init__(self):
-		super(CurrentSensorReadings, self).__init__(**kwargs)
-		self.cols = 1
-		self.add_widget(Label(text="Current Usage"))
-		self.currentLabel = Label(text="7.3 A")
-		self.add_widget(self.currentLabel)
-
-class WeightReadings(GridLayout):
-
-	def __init__(self):
-		super(WeightReadings, self).__init__(**kwargs)
-		self.cols = 1
-		self.add_widget(Label(text="Current Weight on Bit"))
-		self.add_widget(Label(text="87 N"))
-
-class DrillControllApp(App):
-
-	def build(self):
-		layout = GridLayout(cols=2)
-		layout.add_widget(CurrentSensorReadings())
-		layout.add_widget(WeightReadings())
-
-		return layout
-
-DrillControllApp().run()
+root = ControlApp()
+root.master.title('Excavation Control Program')
+root.master.geometry("1000x500")
+root.mainloop()
