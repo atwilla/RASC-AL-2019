94d5a81673c0c80e7d51de9cc95c7dcf266378a7
Alexander Twilla <alexandertwilla@gmail.com>
2019-05-11 19:13:09 -0400

    Write code for determining AC current amplitude. Requires testing.

---
+++ MainMonitoring/MainMonitoring.ino
@@ -0,0 +1,30 @@
+const int currentSensor = A0;
+const float mVPerAmp = 100.0;
+
+float getIMax(int sensorPin) {
+  float maxRead = 0, minRead = 1024, currRead;
+  int start = millis(), limit = (1 / 60) * 1000;
+
+  // Take measurement over 1 period of the current.
+  while (millis() - start <= limit) {
+    currRead = analogRead(sensorPin);
+    
+    // Current sensor outputs voltage such that 512 = 0 Amps. 
+    // Below = negative, above = positive.
+    if (abs(currRead - 512) > maxRead) maxRead = currRead;
+  }
+
+  // Convert analog read to a voltage in mV. Convert that to Amps.
+  float IMax = (maxRead / 1023.0) * 5000 / mVPerAmp;
+  return IMax;
+}
+
+void setup() {
+  Serial.begin(2000000);
+  pinMode(currentSensor, INPUT);
+}
+
+void loop() {
+  float amps = getIMax(currentSensor);
+  Serial.println(amps);
+}
