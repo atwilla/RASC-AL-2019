4f006a1dc7dffe8934704d0f446e4e8b3a891f41
Alex Twilla <alexandertwilla@gmail.com>
2019-05-14 11:06:23 -0400

    Put function definitions in header file to reduce clutter in directory.

--- MainControl/L298N.h
+++ MainControl/L298N.h
@@ -10,21 +10,88 @@
 
 #include "Arduino.h"
 
-class L298N
-{
-  public:
-    //Functions
-    L298N(int A1, int A2, int Aspeed, int B1, int B2, int Bspeed);
-    void DC_Drive(char dir, char motor, int _speed);
-    void DC_Stop(char motor);
+class L298N {
+    private:
+        int dir1PinA;
+        int dir2PinA;
+        int speedPinA;
+        int dir1PinB;
+        int dir2PinB;
+        int speedPinB;
 
-    //Pins
-    int dir1PinA;
-    int dir2PinA;
-    int speedPinA;
-    int dir1PinB;
-    int dir2PinB;
-    int speedPinB;
+    public:
+        L298N(int A1, int A2, int Aspeed, int B1, int B2, int Bspeed){
+
+            pinMode(A1, OUTPUT);
+            pinMode(A2, OUTPUT);
+            pinMode(Aspeed, OUTPUT);
+            pinMode(B1, OUTPUT);
+            pinMode(B2, OUTPUT);
+            pinMode(Bspeed, OUTPUT);
+
+            dir1PinA = A1;
+            dir2PinA = A2;
+            speedPinA = Aspeed;
+            dir1PinB = B1;
+            dir2PinB = B2;
+            speedPinB = Bspeed;
+        }
+
+        void drive(char dir, char motor, int _speed){
+            //Motor A selected.
+            if(motor == 'A' || motor == 'a'){
+
+            //Forward
+                if(dir == 'F' || dir == 'f'){
+                    digitalWrite(dir1PinA, LOW);
+                    digitalWrite(dir2PinA, HIGH);
+                }
+
+                //Reverse
+                if(dir == 'R' || dir == 'r'){
+                    digitalWrite(dir1PinA, HIGH);
+                    digitalWrite(dir2PinA, LOW);
+                }
+
+                //Set speed
+                analogWrite(speedPinA, _speed);
+            }
+
+            //Motor B selected.
+            if(motor == 'B' || motor == 'b'){
+
+                //Set speed
+                analogWrite(speedPinB, _speed);
+
+                //Forward
+                if(dir == 'F' || dir == 'f'){
+                    digitalWrite(dir1PinB, LOW);
+                    digitalWrite(dir2PinB, HIGH);
+                }
+
+                //Reverse
+                if(dir == 'R' || dir == 'r'){
+                    digitalWrite(dir1PinB, HIGH);
+                    digitalWrite(dir2PinB, LOW);
+                }
+            }
+        }
+
+        void stop(char motor){
+            //Motor A selected
+            if(motor == 'A' || motor == 'a'){
+                analogWrite(speedPinA, 0);
+                digitalWrite(dir1PinA, LOW);
+                digitalWrite(dir2PinA, LOW);
+            }
+
+            //Motor B selected
+            if(motor == 'B' || motor == 'b'){
+                analogWrite(speedPinB, 0);
+                digitalWrite(dir1PinB, LOW);
+                digitalWrite(dir2PinB, LOW);
+            }
+        }
 };
 
 #endif
