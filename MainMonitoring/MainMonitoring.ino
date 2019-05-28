c2fc15e1d739ae37845dc0c69996967aef0e7e37
Alexander Twilla <alexandertwilla@gmail.com>
2019-05-27 20:26:22 -0400

    Change method of determining RMS current to use a for loop.

--- MainMonitoring/MainMonitoring.ino
+++ MainMonitoring/MainMonitoring.ino
@@ -1,15 +1,15 @@
 const int currentSensor = A0;
 const int ff1 = A1, ff2 = A2, ff3 = A3, ff4 = A4;
-const float mVPerAmp = 100.0;
+const float mVPerAmp = 125.0;
 
-float getIMax(int sensorPin) {
+float getIRMS(int sensorPin) {
   float maxRead = 0, minRead = 1024, currRead;
-  int start, limit = 17;
+  int start, limit = 170;
 
   start = millis();
 
   // Take measurement over 1 period of the current.
-  while (millis() - start <= limit) {
+  /*while (millis() - start <= limit) {
     currRead = analogRead(sensorPin);
     
     // Current sensor outputs voltage such that 512 = 0 Amps. 
@@ -17,11 +17,32 @@ float getIMax(int sensorPin) {
     if (currRead > maxRead) {
       maxRead = currRead;
     }
+
+    if (currRead < minRead) {
+      minRead = currRead;
+    }
+  }*/
+
+  for (int i = 0; i < 1000; i++) {
+    currRead = analogRead(sensorPin);
+    if (currRead > maxRead) {
+      maxRead = currRead;
+    }
   }
 
   // Convert analog read to a voltage in mV. Convert that to Amps.
-  float IMax = ((maxRead / 1023.0) * 5000 - 2500) / mVPerAmp;
-  return IMax;
+  float IRMS = ((maxRead - 512) / 1023) * 5000 * 0.707 * 0.00165;
+
+  /*if (abs(minRead - 512) > abs(maxRead - 512) || maxRead == 0) {
+    //IRMS = abs(((minRead / 1023.0) * 5000 - 2500) / mVPerAmp);
+    IRMS = 0.0353 * minRead - 18.2;
+    
+  } else {
+    //IRMS = ((maxRead / 1023.0) * 5000 - 2500) / mVPerAmp;
+    IRMS = 0.0353 * maxRead - 18.2;
+  }*/
+  
+  return IRMS;
 }
 
 float getWoB() {
@@ -38,10 +59,12 @@ void setup() {
 }
 
 void loop() {
-  float amps = getIMax(currentSensor), weightOnBit = getWoB();
+  float amps = getIRMS(currentSensor), weightOnBit = getWoB();
   delay(1000);
-  Serial.print('C');
-  Serial.print(amps);
+  Serial.println('C');
+  /*Serial.print(amps);
   Serial.print('W');
-  Serial.print(weightOnBit);
+  Serial.print(weightOnBit);*/
+
+  Serial.println(amps);
 }
